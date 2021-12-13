36a149be5aa9d723f07166b414d615b5bcdca7d8
Milan Rapaić <rapaja@uns.ac.rs>
2021-12-13 18:41:44 +0100

    TD learning

--- src/FiniteMarkovDecisionProcesses/FiniteMarkovDecisionProcesses.jl
+++ src/FiniteMarkovDecisionProcesses/FiniteMarkovDecisionProcesses.jl
@@ -2,8 +2,9 @@ module FiniteMarkovDecisionProcesses
 
 include("_0_mdp.jl")
 include("_1_mdp_utils.jl")
-include("_2_dynamic_programming.jl")
-include("_3_monte_karlo.jl")
-include("_4_temporal_difference.jl")
+include("_2_simulation.jl")
+include("_3_dynamic_programming.jl")
+include("_4_monte_karlo.jl")
+include("_5_temporal_difference.jl")
 
 end # module
